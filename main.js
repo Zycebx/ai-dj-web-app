9400f90891f3b141dc0dc2152f4f7a157b21b64e
Zycebx <142185913+Zycebx@users.noreply.github.com>
2024-07-10 14:22:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 song="";
+scoreleftWrist=0;
+scorerightWrist=0;
 
 leftWristX = 0;
 leftWristY = 0;
@@ -29,6 +31,19 @@ function modelLoaded()
 function draw()
 {
     image(video, 0, 0, 600, 500);
+
+    fill("#3a87cf");
+    stroke("#3a87cf");
+
+    if(scoreleftWrist > 0.2)
+    {
+    circle(leftWristX, leftWristY, 20);
+    inNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(inNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML = "volume = " + volume;
+    song.setVoulume(volume);
+    }
 }
 
 function play()
@@ -43,6 +58,8 @@ function gotPoses(results)
     if(results.length > 0) 
     {
         console.log(results)
+        scoreleftwrist = results[0].pose.keypoints[9].score;
+        console.log("scoreleftWrist = " +scoreleftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " +leftWristX +"leftWristY = " +leftWristY)
